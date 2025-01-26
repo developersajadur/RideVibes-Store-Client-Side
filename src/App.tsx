0c1eb1332332dbd602bae3e0dc01ef842b693ac3
itzmesojib <itzmesojib@gmail.com>
2025-01-26 18:30:54 +0600

    install react, tailwind and  shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <Button className="text-2xl">Hello</Button>
+    </div>
+  );
+};
+
+export default App;
